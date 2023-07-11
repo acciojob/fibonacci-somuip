0b2ab9dac1604e5064a05d1650e729aed0a396f9
somuip <117620797+somuip@users.noreply.github.com>
2023-07-12 00:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	let a =0, b=1;
+	if(n<=2) return num-1;
+	for(let i=0;i<num-2;i++){
+		let temp = b;
+		b = a + b;
+		a = temp;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
